13d55d2f828414c20e95009949add0b844818f6a
webnetkz <dbqqbq@gmail.com>
2022-04-26 16:42:13 +0600

    up

--- Scripts/PingPong.cs
+++ Scripts/PingPong.cs
@@ -16,29 +16,60 @@ public class PingPong : MonoBehaviour {
 	private int playerScore;
 	private int botScore;
 
+  public GameObject menuPanel;
+  public GameObject settingsPanel;
+
 	void Start () 
 	{
 		Reset(0);
 	}
 
+  public void StartMyGame()
+  {
+    ContinueMyGame();
+		ball.GetComponent<Rigidbody2D>().WakeUp();
+		Vector2 direction = new Vector2(1,Random.Range(1.5f, -1.5f)); // Стартавая рандомное направление движения
+		if(Random.Range(0,2) == 1) direction.x *= -1;
+		ball.GetComponent<Rigidbody2D>().AddForce(direction * startBallSpeed);
+  }
+
+  public void ExitMyGame()
+  {
+    Application.Quit();
+  }
+
+  public void PauseMyGame()
+  {
+    menuPanel.SetActive(true);
+    Time.timeScale = 0;
+  }
+
+  public void ContinueMyGame()
+  {
+    menuPanel.SetActive(false);
+    Time.timeScale = 1;
+  }
+
+  public void OpenMySettings()
+  {
+    settingsPanel.SetActive(true);
+    menuPanel.SetActive(false);
+  }
+
+  public void CloseMySettings()
+  {
+    settingsPanel.SetActive(false);
+    menuPanel.SetActive(true);
+  }
+
 	void Update () 
 	{
-    if(Input.GetKey(KeyCode.Escape)) // Выход
+		if(Input.GetKey(KeyCode.Escape)) // Перезагрузка
 		{
-			Application.Quit();
+      PauseMyGame();
+			//Reset(0);
 		}
-    // Начало игры
-		if(Input.GetKeyDown(KeyCode.Space))
-		{
-			ball.GetComponent<Rigidbody2D>().WakeUp();
-			Vector2 direction = new Vector2(1,Random.Range(1.5f, -1.5f)); // Стартавая рандомное направление движения
-			if(Random.Range(0,2) == 1) direction.x *= -1;
-			ball.GetComponent<Rigidbody2D>().AddForce(direction * startBallSpeed);
-		}
-		else if(Input.GetKey(KeyCode.Escape)) // Перезагрузка
-		{
-			Reset(0);
-		}
+
 
     // Управлениея пользователя
 		if(Input.GetAxis("Vertical") > 0 && player.position.y < playerLimitY)
@@ -65,7 +96,7 @@ public class PingPong : MonoBehaviour {
 	public void Reset(float x)
 	{
 		ball.GetComponent<Rigidbody2D>().Sleep();
-		bot.position = new Vector2(bot.position.x, 0);
+		bot.transform.position = new Vector2(bot.transform.position.x, 0);
 		player.position = new Vector2(player.position.x, 0);
 		ball.position = new Vector2(0, 0);
 		if(x > 0) playerScore++; else if(x < 0) botScore++;
--- Scripts/colorChange.cs
+++ Scripts/colorChange.cs
@@ -6,35 +6,11 @@ public class colorChange : MonoBehaviour
     public GameObject cube;
     private Renderer rend;
 
-    // Use this for initialization
-    void Start () 
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update ()
-    {
-      Color newColor = new Color(
-        Random.Range(0f, 1f), 
-        Random.Range(0f, 1f), 
-        Random.Range(0f, 1f)
-      );
-
-      if (Input.GetKeyDown (KeyCode.O))
-          GetComponent<Renderer> ().material.color = newColor;
-      }
-
-    void OnCollisionEnter2D (Collision2D col)
+    private void OnCollisionEnter2D (Collision2D col)
     {
         if (col.collider.name == "Player" || col.collider.name == "Bot")
         {
-          Color newColor = new Color(
-            Random.Range(0f, 1f), 
-            Random.Range(0f, 1f), 
-            Random.Range(0f, 1f)
-          );
-          GetComponent<Renderer> ().material.color = newColor;
+          
         }
     }
 }
