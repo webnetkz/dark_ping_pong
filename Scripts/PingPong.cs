36dc120e07e220a65887462a575024d4aba5ceef
webnetkz <dbqqbq@gmail.com>
2022-05-10 17:18:16 +0600

    Переделана полностью игра под мобильные устройства

--- Scripts/PingPong.cs
+++ Scripts/PingPong.cs
@@ -11,12 +11,13 @@ public class PingPong : MonoBehaviour {
 	public int startBallSpeed = 350;
 	public float playerSpeed = 10;
 	public float botSpeed = 2.5f;
-	public float playerLimitY = 3.5f;
+	public float playerLimitX;
 	public Text playerScoreText;
 	public Text botScoreText;
 	private int playerScore;
 	private int botScore;
   private bool go;
+  private float accelerationX;
 
 	void Start () 
 	{
@@ -25,21 +26,25 @@ public class PingPong : MonoBehaviour {
 
 	void Update () 
 	{
+    accelerationX = Input.acceleration.x;
+    
+
     // Управлениея пользователя
-		if(Input.GetAxis("Vertical") > 0 && player.position.y < playerLimitY)
+		if(Input.acceleration.x > 0.0f && player.position.x < playerLimitX)
+		{
+      Debug.Log(accelerationX);
+			player.Translate(-Vector2.up * playerSpeed * Time.deltaTime);
+		}
+		else if(Input.acceleration.x < 0.0f && player.position.x > -playerLimitX)
 		{
 			player.Translate(Vector2.up * playerSpeed * Time.deltaTime);
 		}
-		else if(Input.GetAxis("Vertical") < 0 && player.position.y > -playerLimitY)
-		{
-			player.Translate(-Vector2.up * playerSpeed * Time.deltaTime);
-		}
 
-    // Условия победы
-		if(ball.position.x < 0)
+    // Управление ботом
+		if(ball.position.y < 0 || ball.position.y > 0)
 		{
-			float Y = Mathf.Lerp(bot.position.y, ball.position.y, botSpeed * Time.deltaTime);
-			bot.position = new Vector2(bot.position.x, Y);
+			float X = Mathf.Lerp(bot.position.x, ball.position.x, botSpeed * Time.deltaTime);
+			bot.position = new Vector2(X, bot.position.y);
 		}
 
 		playerScoreText.text = playerScore.ToString();
@@ -50,8 +55,8 @@ public class PingPong : MonoBehaviour {
 	public void Reset(float x)
 	{
 		ball.GetComponent<Rigidbody2D>().Sleep();
-		bot.transform.position = new Vector2(bot.transform.position.x, 0);
-		player.position = new Vector2(player.position.x, 0);
+		bot.transform.position = new Vector2(bot.transform.position.x, 5.5f);
+		player.position = new Vector2(player.position.x, -5.5f);
 		ball.position = new Vector2(0, 0);
 		if(x > 0) playerScore++; else if(x < 0) botScore++;
 
@@ -62,7 +67,7 @@ public class PingPong : MonoBehaviour {
     if(go)
     {
       ball.GetComponent<Rigidbody2D>().WakeUp();
-      Vector2 direction = new Vector2(1,Random.Range(1.5f, -1.5f)); // Стартавая рандомное направление движения
+      Vector2 direction = new Vector2(Random.Range(1.5f, -1.5f), 1); // Стартавая рандомное направление движения
       if(Random.Range(0,2) == 1) direction.x *= -1;
       ball.GetComponent<Rigidbody2D>().AddForce(direction * startBallSpeed);
     }
