0ecb69f09cb66f2002bde6cfb58554c32c54c5e5
webnetkz <dbqqbq@gmail.com>
2022-05-25 17:23:12 +0600

    крупное обновление, много багов пофикшено, переписана большая часть кода

--- Scripts/PingPong.cs
+++ Scripts/PingPong.cs
@@ -8,9 +8,9 @@ public class PingPong : MonoBehaviour {
 	public Transform player;
 	public Transform bot;
 	public Transform ball;
-	public int startBallSpeed = 350;
-	public float playerSpeed = 10;
-	public float botSpeed = 2.5f;
+	public int startBallSpeed;
+	public float playerSpeed;
+	public float botSpeed;
 	public float playerLimitX;
 	public Text playerScoreText;
 	public Text botScoreText;
@@ -25,20 +25,12 @@ public class PingPong : MonoBehaviour {
 		Reset(0);
 	}
 
-	void Update () 
+	void Update ()  
 	{
     accelerationX = Input.acceleration.x;
-
-    //Debug.Log(accelerationX);
-    Debug.Log(accelerationX);
-
     playerPositionX = accelerationX * playerLimitX;
 
-
     player.position = new Vector2(playerPositionX, player.position.y);
-
-    // Управлениея пользователя
-
 
     // Управление ботом
 		if(ball.position.y < 0 || ball.position.y > 0)
@@ -54,20 +46,20 @@ public class PingPong : MonoBehaviour {
   // Перезагрузка
 	public void Reset(float x)
 	{
-		ball.GetComponent<Rigidbody2D>().Sleep();
-		bot.transform.position = new Vector2(bot.transform.position.x, 5.5f);
-		player.position = new Vector2(player.position.x, -5.5f);
-		ball.position = new Vector2(0, 0);
-		if(x > 0) playerScore++; else if(x < 0) botScore++;
+		ball.GetComponent<Rigidbody2D>().Sleep(); // Останавливаем шар
+		bot.transform.position = new Vector2(bot.transform.position.x, 14f); // Выставляем на стартовую позицию бота
+		player.position = new Vector2(player.position.x, -14f); // Выставляем на стратовую позицию игрока
+		ball.position = new Vector2(0, 0); // Выставляем на стартовую позицию мяч
+		if(x > 0) playerScore++; else if(x < 0) botScore++; // В завизимости от переданного значения в этот медод, добавляем очко боту или игроку
 
-    if(playerScore == 10 || botScore == 10) {
+    if(playerScore == 10 || botScore == 10) { // Условия завершения игровой сессии
       SceneManager.LoadScene(0);
     }
 
     if(go)
     {
       ball.GetComponent<Rigidbody2D>().WakeUp();
-      Vector2 direction = new Vector2(Random.Range(1.5f, -1.5f), 1); // Стартавая рандомное направление движения
+      Vector2 direction = new Vector2(Random.Range(1f, -1f), 1); // Стартавая рандомное направление движения
       if(Random.Range(0,2) == 1) direction.x *= -1;
       ball.GetComponent<Rigidbody2D>().AddForce(direction * startBallSpeed);
     }
